9424da4697c9d690985c94c5267a3601448969a9
YaSol <1547471276@qq.com>
2023-02-10 17:07:51 +0800

    rebase commit
    rebase commit

--- src/App.tsx
+++ src/App.tsx
@@ -11,8 +11,7 @@ function App() {
       <span className='three'>目前是第三次修改</span>
       <span className='five'>目前是第五次修改（第四次被reset）</span>
       <span>第六次提交</span>
-      <span>第七次提交</span>
-      <span>第八次提交</span>
+      <span>revert第七次修改</span>
     </div>
   )
 }
